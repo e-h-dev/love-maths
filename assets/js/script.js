eb20a96d3f817f36e880800ecbe91732d20b22cd
E Hammond <ebaal8226@gmail.com>
2024-03-10 12:20:52 +0000

    add code for multiply questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,9 @@ function runGame(gameType){
     let num2 = Math.floor(Math.random() *25) +1;
 
     if (gameType === "addition"){
-        displayAdditionQuestion(num1, num2)
+        displayAdditionQuestion(num1, num2);
+    } else if(gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -56,6 +58,8 @@ function calculateCorrectAnswer(){
 
     if (operator === '+'){
         return [operand1 + operand2, 'addition']
+    } else if (operator === 'x') {
+        return [operand1 * operand2, 'multiply']
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator} Aborting!`;
@@ -83,6 +87,8 @@ function displaySubtractQuestion(){
     
 }
 
-function displayMultiplyQuestion(){
-    
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById('operand-1').textContent = operand1;
+    document.getElementById('operand-2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x';
 }
